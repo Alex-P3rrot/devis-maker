4824b02906cdcfb707b475bbcc126d053a09f795
perrot.alex.dev <perrot.alex.dev@gmail.com>
2023-06-26 15:16:12 +0200

    Header & content OK

--- index.php
+++ index.php
@@ -2,8 +2,22 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$mpdf = new \Mpdf\Mpdf();
 
-$mpdf->WriteHTML('Hello World');
+$header = file_get_contents('./templates/header.html');
+$content = file_get_contents('./templates/content.html');
+
+
+$mpdf = new \Mpdf\Mpdf([
+    'mode' => 'utf-8',
+    'format' => 'a4'
+]);
+$mpdf->useSubstitutions=false;
+$mpdf->setAutoTopMargin = 'stretch';
+$mpdf->SetDisplayMode('fullpage');
+$mpdf->SetDefaultBodyCSS('background', "url('./assets/devis-background.png')");
+$mpdf->SetDefaultBodyCSS('background-image-resize', 6);
+
+$mpdf->SetHTMLHeader($header);
+$mpdf->WriteHTML($content);
 
 $mpdf->Output();
